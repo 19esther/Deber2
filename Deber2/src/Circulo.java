648503cb5a9baeab4aba956926de71ab903e1692
19esther <thefashon.tkm@hotmail.com>
2015-11-24 20:42:11 -0500

    original.

---
+++ Deber2/src/Circulo.java
@@ -0,0 +1,28 @@
+
+public class Circulo {
+	/*Crear una clase Circulo con el atributo radio, y el metodo getArea().
+	  Crear dos constructores, el primero sin argumentos, el segundo con
+	   el parametro de entrada radio
+	 */
+		private double radio;
+		public Circulo(){
+			
+		}
+		public  Circulo(double radioParam){
+			this.radio=radioParam;
+		}
+		public void  setRadio(double radioParam){
+			this.radio=radioParam;
+}
+		
+			public double getRadio(){
+				return this.radio;
+		}
+			public double getArea(){
+				return Math.PI*radio*radio;
+			}
+	}
+
+// NOEMI ESTHER ALAVA
+
+
